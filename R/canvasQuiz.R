de2a7d1154dbf8621a87790c21a592bb2979d643
Duncan Temple Lang <duncan@r-project.org>
2021-05-09 11:45:25 -0700

    added the schema. Moved to representing each question as a list of components - prompt, responses, answer, metadata and label/title/ident

--- R/canvasQuiz.R
+++ R/canvasQuiz.R
@@ -4,15 +4,15 @@ read =
     # If you do use xmlParse(), then the document will have a default namespace and we need to specify that
     # in the XPath queries.
     #
-function(file, meta = TRUE, doc = htmlParse(file))
+function(file, doc = htmlParse(file))
 {
     qs = getQuestions(doc)
     metad = getMetadata(qs = qs)
     ans = mapply(processQuestion, qs, metad, SIMPLIFY = FALSE)
-    if(meta)
-        list(answers = ans, meta = metad)
-    else
-        ans
+#    if(meta)
+#        list(answers = ans, meta = metad)
+#    else
+#        ans
 }
 
 getMetadata =
@@ -22,17 +22,29 @@ function(doc, qs = getQuestions(doc))
 }
 
 getQuestions =
+    #
+    # only the questions that have a resprocessing node, so ignore the 
+    # "text only" 'Questions'.
+    #
 function(doc)
 {
     q = getNodeSet(doc, "//item[@title = 'Question' and .//resprocessing]")
-    names(q) = sapply(q, xmlGetAttr, "ident")
+    names(q) = sapply(q, getQuestionName)
     q
 }
 
+getQuestionName =
+function(q)
+{
+    xmlGetAttr(q, "label", xmlGetAttr(q, "title", xmlGetAttr(q, "ident")))
+}
+
 processQuestion =
+    #
+    # Old version, overwritten below.
+    #
 function(q, meta = getQMetadata(q), prompt = TRUE)
 {
-browser()    
     ansId = xpathSApply(q, ".//respcondition//varequal[starts-with(@respident, 'response')]", xmlValue)
     if(length(ansId)) {
         xquery = sprintf(".//response_label[ %s ]", paste(sprintf("@ident = '%s'", ansId), collapse = " or "))
@@ -51,6 +63,55 @@ browser()
     
     ans
 }
+
+
+processQuestion =
+function(q, meta = getQMetadata(q), prompt = TRUE)
+{
+    ans = list()
+    ans$prompt = xpathSApply(q, ".//presentation/material/mattext", xmlValue)
+    ans$responses = xpathApply(q, ".//presentation/response_lid", getResponses, meta)
+    ans$answer = getAnswer(q, meta)
+    ans$meta = meta
+    ans$info = xmlAttrs(q)
+    
+    ans
+}
+
+getResponses =
+function(r, meta)
+{
+   xpathSApply(r, ".//response_label", function(x) structure(xmlValue(x), names = xmlGetAttr(x, "ident")))
+}
+
+getAnswer =
+    #
+    # Operate on the resprocessing node.
+    # These have 1 outcomes and 1 or more respcondition nodes.
+    # With formula questions, there may be an itemproc_extension sibling node of resprocessing.
+    # The outcomes appears to be a sequence of one possibly more <decvar> nodes and these nodes have only attributes
+    #   mavalue, minvalue, varname, vartype
+    # See s[[1]]$decvarType
+    #
+    # The respcondition nodes have a conditionvar and setvar
+function(q, meta)
+{
+# browser()   
+    ansId = xpathSApply(q, ".//respcondition//varequal[starts-with(@respident, 'response')]", xmlValue)
+    if(length(ansId)) {
+        xquery = sprintf(".//response_label[ %s ]", paste(sprintf("@ident = '%s'", ansId), collapse = " or "))
+        ans = xpathSApply(q, xquery, xmlValue)
+        if(length(ans) == 0)
+            ans = ansId
+    } else
+        ans = ""
+    score = xpathSApply(q, ".//respcondition/setvar[@varname = 'SCORE' and @action = 'Set']", xmlValue)
+    if(length(score) == 0)
+        score = NA
+
+    list(answer = ans, score = score)
+}
+
 
 getQMetadata =
 function(q)
--- README.md
+++ README.md
@@ -2,3 +2,11 @@ This is a quick exploration of reading the XML that Canvas generates when export
 I am not very familiar with Canvas quizzes and the range of possible question types and content. 
 So this is based on looking at 2 sample quizzes and currently limited to those, with some hopefully
 generalizations that apply to others.
+
+The schema is at [http://www.imsglobal.org/xsd/ims_qtiasiv1p2p1.xsd](http://www.imsglobal.org/xsd/ims_qtiasiv1p2p1.xsd)
+The format may be documented [here](http://www.imsglobal.org/question/index.html) but this seems
+slightly different.
+
+The exams package might have some data structures to represent questions (or it may just translate directly.)
+
+
